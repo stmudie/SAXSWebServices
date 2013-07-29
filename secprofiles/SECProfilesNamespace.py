a60027dd85e71470aa38d7dbc55b3ddfb85532d9
Stephen Mudie <Stephen.Mudie@synchrotron.org.au>
2013-07-29 16:02:02 +1000

    Selectable SEC.

--- secprofiles/SECProfilesNamespace.py
+++ secprofiles/SECProfilesNamespace.py
@@ -15,6 +15,7 @@ class SECProfilesNamespace(BaseNamespace):
         self.bufferrange = [-1,-1]
         self.avBufferDat = DatFile()
         self.avSampleDat = DatFile()
+        self.activeFile = ''
     
     def sendSAXSProfile(self, name, data):
         filename = basename(data['filename'])
@@ -28,7 +29,7 @@ class SECProfilesNamespace(BaseNamespace):
         self.bufferrange = bufferrange
         
         bufferDats = []
-        filename = splitext(r.get('pipeline:sec:filename'))[0]
+        filename = splitext(self.activeFile)[0]
         bufferNames = ['{}/raw_dat/{}_{}.dat'.format(dirname(dirname(filename)),basename(filename),str(num).zfill(4)) for num in range(bufferrange[0],bufferrange[1])]
         
         count = 0
@@ -43,7 +44,7 @@ class SECProfilesNamespace(BaseNamespace):
         
         profiles = []
         
-        filename = splitext(r.get('pipeline:sec:filename'))[0]
+        filename = splitext(self.activeFile)[0]
         
         for profileNumber in range(data['range'][0],data['range'][1]):
             if (data['subtract'] == False):
@@ -61,7 +62,7 @@ class SECProfilesNamespace(BaseNamespace):
     
     def on_Load_Profile(self, data):
         
-        filename = splitext(r.get('pipeline:sec:filename'))[0]
+        filename = splitext(self.activeFile)[0]
         profileNumber = data['position']
         
         if (data['subtract'] == False):
@@ -83,7 +84,7 @@ class SECProfilesNamespace(BaseNamespace):
         
         self.updateAverageBuffer(data['buffer'])
 
-        filename = splitext(r.get('pipeline:sec:filename'))[0]
+        filename = splitext(self.activeFile)[0]
         sampleDats = []       
         sampleNames = ['{}/raw_dat/{}_{}.dat'.format(dirname(dirname(filename)),basename(filename),str(num).zfill(4)) for num in range(data['sample'][0],data['sample'][1])]
         
@@ -104,13 +105,13 @@ class SECProfilesNamespace(BaseNamespace):
     
     def on_SaveAverage(self, filename):
         print 'save'
-        rawfilename = splitext(r.get('pipeline:sec:filename'))[0]
+        rawfilename = splitext(self.activeFile)[0]
         self.avSampleDat.save('{}/analysis/{}.dat'.format(dirname(dirname(rawfilename)),filename))
     
     def sendProfile(self, name, filter_on_quality = 0):
-        filename = r.get('pipeline:sec:filename')
+        
         try:
-            data = pickle.loads(r.get('pipeline:sec:Rg'))
+            data = pickle.loads(r.get('pipeline:sec:{}:Rg'.format(self.activeFile)))
         except TypeError:
             self.emit('ErrorMessage',{'title': "Error", 'message': "No data in database."})
             return
@@ -118,31 +119,45 @@ class SECProfilesNamespace(BaseNamespace):
         namedict = {'Rg_Array': 1, 'I0_Array' : 2, 'Quality' : 3, 'HighQ_Array' : 4}
         for n in name:
             array =[(element[0],element[namedict[n]]) for element in data['profiles'] if element[namedict['Quality']] >= 0]
-            self.emit(n, {'filename': filename, 'profile':array})
+            self.emit(n, {'filename': self.activeFile, 'profile':array})
 
     def checkForNewRedisRgProfile(self):
         
         self.sub = r.pubsub()
-        self.sub.subscribe('pipeline:sec:pub:Rg')
+        self.sub.subscribe('pipeline:sec:pub:Filename')
         print 'listening'
         lastTimeSent = time()
         for message in self.sub.listen():
-            print 'Message from pipeline:sec:pub:Rg'
+            print 'Message from pipeline:sec:pub:Filename'
             if (message['type'] != 'message'):
                 print 'Wrong message type: %s' %(message['type'],)
                 continue
             try:
                 if time()-lastTimeSent < 0.5:
                     continue
-                #data = pickle.loads(r.get('pipeline:sec:Rg'))
-                self.sendProfile(['Rg_Array','I0_Array'])
+                if not r.sismember("pipeline:sec:filenames", message['data']):
+                    self.updateFileList()
+                if message['data'] == self.activeFile:
+                    self.sendProfile(['Rg_Array','I0_Array'])
                 lastTimeSent = time()
             except Exception, e:
                 print 'There was an exception in checkForNewRedisRgProfile: %s' %(e,)
 
+    def on_LoadFile(self, filename):
+        self.bufferrange = [-1,-1]
+        self.avBufferDat = DatFile()
+        self.avSampleDat = DatFile()
+        
+        self.activeFile = filename
+        self.sendProfile(['Rg_Array','I0_Array','HighQ_Array'])
+    
+    def updateFileList(self, ):
+        print 'update'
+        self.emit('File_List',list(r.smembers("pipeline:sec:filenames")))
+    
     def recv_connect(self):
         print 'connect Rg'
-        self.sendProfile(['Rg_Array','I0_Array','HighQ_Array'])
+        self.updateFileList()
         g = self.spawn(self.checkForNewRedisRgProfile)
         
     
