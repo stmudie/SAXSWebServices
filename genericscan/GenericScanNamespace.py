a87e08939497e380bf3ffd60590a7a4de59e8ece
ics <stmudie>
2013-08-16 17:47:11 +1000

    Getting working on beamline

--- genericscan/GenericScanNamespace.py
+++ genericscan/GenericScanNamespace.py
@@ -12,6 +12,12 @@ def xstr(s):
     else:
         return s
 
+def running_sum(a):
+    tot = 0
+    for item in a:
+        tot += item
+        yield tot
+
 class GenericScanNamespace(BaseNamespace):
     def on_connect(self):
         print 'connect'
@@ -45,54 +51,82 @@ class GenericScanNamespace(BaseNamespace):
         self.emit('scandata', epn, scan, data)
 
     def run(self,epn,scan,type='all'):
-        filenames = []
         
+        #Load scan data from redis
         data = pickle.loads(r.get('generic:' + epn + ':scan:' + scan))
         
-        for pos3 in range(0,data['number'][2]):
-            for pos2 in range(0,data['number'][1]):
-                for pos1 in range(0,data['number'][0]):
+        # Base PVS
+        indexPV = "13INDEXARRAY:array"
+        IOCPV = 'SR13ID01HU02IOC02:'
+        result = 0
+                
+        #Setup filenames
+        filenames = []
+        num3 = int(data['number'][2] or 1)
+        num2 = int(data['number'][1] or 1)
+        num1 = int(data['number'][0] or 1)
+        
+        for pos3 in range(num3):
+            for pos2 in range(num2):
+                for pos1 in range(num1):
                     #position = pos1 + pos2*posData[0].length + pos3*posData[1].length*posData[0].length
                     filenames.append(xstr(data['filenames'][0][pos1]) + xstr(data['filenames'][1][pos2]) + xstr(data['filenames'][2][pos3]))
                     
-        filenameString = "".join(filenamess)
-        filenameLen = [len(name) for name in filenames]
-        
-        indexPV = "13INDEXARRAY:array"
-        
-        # Setup global scan record parameters
-        IOCPV = 'SR13ID01HU02IOC02:'
-        
-        
-        result = 0
+        filenameString = "".join(filenames)
+        filenameLen = list(running_sum([len(name) for name in filenames]))
+
+        result += caput('%s1:arrayIndices' %(indexPV, ), filenameLen)
+        result += caput('%s1:arrayValues' %(indexPV, ), filenameString)
+
+        # Setup global scan record parameters        
         tableCount = 0
-        
+
         for loop in range(1,4):
             scanPV = 'SR13ID01HU02IOC02:scan%d.' % (loop,)
+            
             #Clear SCAN
             result += caput(scanPV+'CMND',3)
             result += caput(scanPV+'PDLY',0)
+            
             #Detector Triggers
             if loop == 1 :
                 result += caput(scanPV+'T1PV','13PIL1:cam1:Acquire')
             else :
                 result += caput(scanPV+'T1PV','SR13ID01HU02IOC02:scan%d.EXSC' % (loop-1,))
             
-            for posNum in range(0,3) :
-                absPos = loop*3+posNum
+            #Set number of points
+            number = int(data['number'][loop-1] or 0)
+            result += caput(scanPV+'NPTS', number)
+            
+            #Set Delay
+            result += caput(scanPV+'DDLY', int(data['delay'][loop-1] or 0))
+            
+            #Index Array Positioner
+            result += caput(scanPV+'R4PV', indexPV + ':arrayIndex' + str(loop))
+            result += caput(scanPV+'P4PV', indexPV + ':arrayIndex' + str(loop))
+            result += caput(scanPV+'P4SM', 0)
+            result += caput(scanPV+'P4SP', 0)
+            result += caput(scanPV+'P4EP', number-1)            
+            
+            for posNum in range(3) :
+                absPos = (loop-1)*3+posNum
+                
+                if not data['positioners'][absPos] :
+                    continue
+                
+                print data['positioners']
+                
                 start = data['start'][absPos]
                 end = data['end'][absPos]
-                number = data['number'][absPos]
                 scantype = data['type'][absPos]
                 
-                result += caput(scanPV+'R'+str(1+posNum)+'PV', data['positioners'][absPos])
-                result += caput(scanPV+'P'+str(1+posNum)+'PV', data['positioners'][absPos])
+                result += caput(scanPV+'R'+str(1+posNum)+'PV', data['positioners'][absPos]['PV'])
+                result += caput(scanPV+'P'+str(1+posNum)+'PV', data['positioners'][absPos]['PV'])
                 
                 if scantype == 'Linear':
                     result += caput(scanPV+'P'+str(1+posNum)+'SM', 0)
                     result += caput(scanPV+'P'+str(1+posNum)+'SP', start)
                     result += caput(scanPV+'P'+str(1+posNum)+'EP', end)
-                    result += caput(scanPV+'P'+str(1+posNum)+'NPTS', number)
                 
                 elif scantype == 'Exponential' :
                     prefactor = (end-start)/((number-1)*(step**(number-1)));
@@ -102,34 +136,20 @@ class GenericScanNamespace(BaseNamespace):
                 
                 elif scantype == 'Table' :
                     result += caput(scanPV+'P'+str(1+posNum)+'SM', 1)
-                    result += caput(scanPV+'P'+str(1+posNum)+'PA', data['table'][tableCount])
+                    result += caput(scanPV+'P'+str(1+posNum)+'PA', data['tableData'][tableCount])
                     tableCount = tableCount + 1
                     
                 else:
                     pass  
 
+                
+
+
         result += caput(indexPV + ':arrayIndex1',0)
+        result += caput(indexPV + ':arrayIndex2',0)
+        result += caput(indexPV + ':arrayIndex3',0)
         
-        
-        
-        #if result != 9 :
-        #    print "Something wrong setting " + str(9-result) + " PVs. Continuing anyway."
-        #
-        # Setup positioners for proteins
-        #result = 0
-        #positioner = ['SR13ID01SYR01:SMPL_RAW_COORD','SR13ID01SYR01:WASH_TYPE','SR13ID01HU02IOC04:SMPL_TYPE']
-        #dictKey = ['COORD','WASH','TYPE']
-        #data = {'COORD': positions, 'WASH': washes, 'TYPE': types}
-        #for posNum in range(3):
-        #    result += caput(scanPV+'R'+str(1+posNum)+'PV', positioner[posNum])
-        #    result += caput(scanPV+'P'+str(1+posNum)+'PV', positioner[posNum])
-        #    result += caput(scanPV+'P'+str(1+posNum)+'SM', 1)
-        #    time.sleep(0.1)
-        #    result += caput(scanPV+'P'+str(1+posNum)+'PA', data[dictKey[posNum]])
-        #    result += caput(scanPV+'NPTS', len(positions))
-        #if result != 13 :
-        #    print "Something wrong setting " + str(15-result) + " some PVs. Continuing anyway."
-        #
+     
         # Setup sample name and concentration positioners
         #result = 0
         #result += caput(indexPV+'1:arrayValues', str(sampleNameString))
