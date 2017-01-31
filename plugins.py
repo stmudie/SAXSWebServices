0a0787f9497a4699c6cb96b288e6847288471135
Stephen Mudie <stephen.mudie@synchrotron.org.au>
2017-01-31 22:11:33 +1100

    More removing stuff

--- plugins.py
+++ plugins.py
@@ -1,14 +1,17 @@
 from functools import wraps
-from flask.ext.vbl import VBL
-from flask.ext.beamline import Beamline
+#from flask.ext.vbl import VBL
+#from flask.ext.beamline import Beamline
 
-vbl = VBL()
-beamline = Beamline()
+#vbl = VBL()
+#beamline = Beamline()
+
+vbl = ''
+beamline = ''
 
 def beamline_or_vbl(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        if not (vbl.current_user or beamline.current):
-            return vbl.get_login_redirect()
+        # if not (vbl.current_user or beamline.current):
+        #    return vbl.get_login_redirect()
         return f(*args, **kwargs)
     return decorated
